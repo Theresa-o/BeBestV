2e5063e205e51820a1f665770922fd6f6319b454
Theresa-o <okorotheresa19@gmail.com>
2022-07-19 11:17:40 +0100

    first commit

---
+++ src/components/Task.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="task">
+        <h3>
+            {{ task.title }}
+            <i class="fas fa-times"></i>
+        </h3>
+        <p>{{ task.created_at }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Task',
+    props: {
+        task: Object,
+    },
+}
+</script>
+
+<style scope>
+.fas {
+    color: red;
+}
+
+.task {
+    background: #f4f4f4;
+    margin: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+}
+
+.task h3 {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+</style>
